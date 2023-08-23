7a242ad3a7df0af1ccaff9c127bb4dea839f28d0
Priangshu Das <88083475+priangshudas@users.noreply.github.com>
2023-08-23 20:34:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x-10+"px"
+    crsr.style.top = dets.y-10+"px"
+    blur.style.left = dets.x-150+"px"
+    blur.style.top = dets.y-150+"px"
+})
+
+gsap.to("#nav",{
+    backgroundColor: "#000",
+    height: "90px",
+    duration: 1,
+    scrollTrigger:{
+        trigger: "#nav",
+        scroller: "body",
+        // markers: true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 0.5
+
+    }
+})
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger: "#main",
+        scroller: "body",
+        start: "top -25v%",
+        end: "top -70%",
+        scrub: 1
+    }
+})
